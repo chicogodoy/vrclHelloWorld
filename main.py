01411a31338c1e2262f87c6d06594c1c25092f10
Francisco Godoy <chicogodoy.evo@gmail.com>
2026-04-29 22:32:22 -0300

    Inclusão do INDEX.HTML

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-def main():
-    print("Olá, mundo!")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+def get_data():
+    return {
+        "titulo": "Meu Resultado",
+        "mensagem": "Olá, mundo!",
+        "data": "29 de abril de 2026",
+        "itens": ["Item 1", "Item 2", "Item 3"]
+    }
+
+@app.route("/")
+def index():
+    dados = get_data()
+    return render_template("index.html", **dados)
 
 if __name__ == "__main__":
-    main()
-
-print(__name__)
+    app.run(debug=True, extra_files=['templates/index.html'])
